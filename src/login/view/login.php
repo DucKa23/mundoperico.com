6d322ae318ff6d60b19cc1f008464c1b9524ce94
DucKa23 <50711395+DucKa23@users.noreply.github.com>
2019-09-18 18:32:48 +0200

    Add files via upload

--- src/login/view/login.php
+++ src/login/view/login.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title> mundoPerico - Log in</title>
+	<title> mundoPerico - Login</title>
+		<meta charset="utf-8">
+	
+
 </head>
 <body>
 
-Esta es la pagina de Log in 
+<form class="login">
+  <div class="form-group">
+    <input type="text"  name="usuario" placeholder="Escribe tu Usuario">
+    <input type="password"  name="pwd" placeholder="Escribe tu Contraseña">
+  </div>
+  <br/>
+  <input type="button" class="btn" value="Login">     
+</form>
 
 </body>
 </html>
