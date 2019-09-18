209e8c4d18aea3a20dce8ea9ddf7ecf7b1a2f0ac
DucKa23 <50711395+DucKa23@users.noreply.github.com>
2019-09-18 20:29:45 +0200

    Add files via upload

--- src/Contact/view/contact.php
+++ src/Contact/view/contact.php
@@ -5,7 +5,7 @@
 		<title>Contacto</title>
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="css/index.css">
-		<!-- Framework Jquery-->
+
 		<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
 		<script type="text/javascript" src="js/index.js"></script>
 		<script src="https://use.fontawesome.com/eaf3553229.js"></script>
@@ -17,7 +17,7 @@
 			<input type="email" id="email" name="email" placeholder="Email"><br>
 			<textarea type="text" id="mensaje" name="mensaje" placeholder="Escribe aqui" rows="7" cols="30"></textarea>
 			<input type="button" id="btn" value="Enviar su mensaje">
-			<!--Caja de mensajes de validación y respuesta del server -->
+		
 			<div id="data"></div>
 		</form>
 	</body>
